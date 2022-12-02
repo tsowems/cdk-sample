19a811c85959b0ec9b29880a02754954741dd4f0
tsowems <tsowems@gmail.com>
2022-12-02 13:52:00 +0000

    fix text

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,4 +1,4 @@
 export const handler = async (event: {name: string}) => {
-    const result: string = event.name ? `Good Job ${event.name}!` : `Good Job`;
+    const result: string = event.name ? `Good Job ${event.name}!` : `Good Job!`;
     return result;
 }
